953d492f3bab7a4d3d679d8ace82bb865e6a3f41
LisaFalichatulIbriza22 <Ichaibriza66@gmail.com>
2024-05-09 11:58:40 +0700

    10

---
+++ dart_project/bin/Latihan2/10.dart
@@ -0,0 +1,7 @@
+void main() {
+  List<String> daftarKata = ["apel", "banana", "ceri", "durian", "anggur"];
+
+  for (var kata in daftarKata) {
+    print("Kata: $kata, Panjang: ${kata.length}");
+  }
+}
